a82a4bfdbd812bb77809a8b6d0a0632d634932a2
Thirugnanam, Vijayananthan <Vijayananthan.Thirugnanam@anz.com>
2019-08-26 07:47:43 +0800

    first commit

---
+++ packages/input/src/index.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    box-sizing: border-box;
+    font-size: 26px;
+    font-weight: 300;
+    padding: 10px 40px 10px 10px;
+    width: 150px;
+`;
+
+export default Input;
+
+
+export function showInput() {
+    console.log("showInput");
+}
